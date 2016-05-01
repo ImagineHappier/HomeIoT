2b0e7096562936d1334ea03e7bda75810b4d4bfc
haanmo <haanmo@naver.com>
2016-04-30 19:54:27 -0500

    Added files via upload

---
+++ Cloud_IH_Client/api/hue_3_turn_off.py
@@ -0,0 +1,17 @@
+import json
+import requests
+import time
+
+ip="http://192.168.0.8"
+headers ={'Content-type':'application/json','Accept':'text/plain'}
+
+url = ip+'/api/newdeveloper/lights/3/state'
+try:
+	pdata = {"on":False, "sat":255, "bri":255,"hue":1000}
+	r = requests.put(url,data=json.dumps(pdata),headers=headers)
+        print r.text
+
+except KeyboardInterrupt:
+        print "End"
+
+
